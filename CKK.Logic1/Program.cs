6635daf4e812548716f1ad395b2dcf4ef698db9d
d3m0ni <demoni.senza@gmail.com>
2022-07-28 17:21:27 -0600

    Add project files.

---
+++ CKK.Logic1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CKK.Logic1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
